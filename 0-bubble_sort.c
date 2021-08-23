6987a7d079d177e7c525a674d93b8c34e89069c7
Chris Vanndy <2736@holbertonschool.com>
2021-08-23 15:44:00 -0500

    task 0 / header
    trying to stage on gitdesktop

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/** 
+ * bubble_sort - sorts members in an array pair by pair
+ * @size: the size of the array being sorted
+ * @array: the array being passed
+ * Return: int 0
+ */
+ 
+ void bubble_sort(int *array, size_t size)
+ {
+    size_t i, j, temp;
+
+    if (array != NULL)
+    {
+      for (i = 0; i < (size -1); i++)
+      {
+        for (j = 0; j < (size - i - 1); j++)
+        {
+          if (array[j] > array[j + 1])
+          {
+            // could make a function for this
+            temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+            print_array(array, size);
+          }
+        }
+      }
+    }
+ } 
