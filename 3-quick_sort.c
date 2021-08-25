0fcb892815636ab20ccdc401351c8a15077d260d
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-25 22:11:31 +0000

    edits numbers and increments, organized functions, pushing for checker test

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,39 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-
-void quick_sort(int *array, size_t size)
-{
-    if (!array)
-        return;
-    
-    quicksort(array, 0, size - 1, size);
-     
-}
-
 void swap(int *i, int *j)
 {
     int tmp = *i;
     *i = *j;
     *j = tmp;
 }
-
 int partition(int *arr, int l, int r)
 {
     int pivot = arr[r];
-    int i = l;
+    int i = l - 1;
     int j;
     for (j = l; j < r; j++) {
-        if (arr[j] < pivot) {
-            swap(&arr[i], &arr[j]);
-            i++;    
+        if (arr[j] <= pivot) {
+            if (i != j)
+            {
+                i++;
+            }
+            swap(&arr[i], &arr[j]);  
         }
     }
-    swap(&arr[i], &arr[r]);
+    swap(&arr[i + 1], &arr[r]);
     
-    return i;
+    return (i + 1);
 }
-
 void quicksort(int *arr, int l, int r, size_t size)
 {
     int i;
@@ -44,7 +35,16 @@ void quicksort(int *arr, int l, int r, size_t size)
     
     
     quicksort(arr, l, i - 1, size);
-    print_array(arr, sizeof(arr) + 2);
+    print_array(arr, size);
     quicksort(arr, i + 1, r, size);
     
+    
+}
+void quick_sort(int *array, size_t size)
+{
+    if (!array)
+        return;
+    
+    quicksort(array, 0, size - 1, size);
+     
 }
